5ba75ca25685a9df4bfdf0afa59ad9ab9c373d8a
blackbrokkoli <pluemer_subs@fastmail.com>
2020-03-29 22:05:24 +0200

    marens edits

--- button.py
+++ button.py
@@ -3,12 +3,30 @@ from time import sleep     # this lets us have a time delay (see line 15)
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    # set GPIO25 as input (button)  
   
-try:  
+try:
+    Button = 0
+    ButtonPressed = 0
+    ButtonReleased = 0
     while True:            # this will carry on until you hit CTRL+C  
-        if GPIO.input(25): # if port 25 == 1  
-            print "Port 25 is 1/HIGH/True - LED ON"  
+        if GPIO.input(25): # if port 25 == 1
+            if Button is 0:
+                ButtonPressed = 1
+            else:
+                ButtonPressed = 0
+            Button = 1
+            print "Port 25 is 1/HIGH/True - LED ON"
         else:  
+            if Button is 1:
+                ButtonReleased = 1
+            else:
+                ButtonReleased = 0
+            Button = 0
             print "Port 25 is 0/LOW/False - LED OFF"  
+        
+        if ButtonPressed:
+            print "Button presssed"
+        if ButtonReleased:
+            print ButtonReleased
         sleep(0.1)         # wait 0.1 seconds  
   
 finally:                   # this block will run no matter how the try block exits  
